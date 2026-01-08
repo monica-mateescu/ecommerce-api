282bba80ef1660ccd9a96c3d373a5194f2791c0d
Simone Massonie <simone.massonie@gmail.com>
2026-01-08 17:46:28 +0100

    add validateBodyZod

---
+++ src/middlerware/validateMiddleware.ts
@@ -0,0 +1,15 @@
+import type { Request, Response, NextFunction } from "express";
+import { z, ZodObject } from "zod/v4";
+
+export function validateBodyZod(zodSchema: ZodObject) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { data, error, success } = zodSchema.safeParse(req.body);
+
+    if (!success) {
+      next(new Error(z.prettifyError(error), { cause: 400 }));
+    } else {
+      req.body = data;
+      next();
+    }
+  };
+}
