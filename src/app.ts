43850785e2b1cbbd1a31c49344e7a22e4b84dbe2
Daria Kozlova <kozloffa.daria@gmail.com>
2026-01-08 20:28:43 +0100

    Add CORS support to Express app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import "#db";
 import {errorHandler} from "#middlerware";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(express.json());
+
+// To allow a front-end application to access APIs from a different origin during development
+app.use(cors());
 
 app.get("/", (req, res) => res.send("eCommerce API is running!"));
 
