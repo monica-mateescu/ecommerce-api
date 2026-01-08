9b0667a5a320ec4685e4792d5683cae8967a0610
Daria Kozlova <kozloffa.daria@gmail.com>
2026-01-08 20:24:59 +0100

    Register error handling middleware in app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import "#db";
+import {errorHandler} from "#middlerware";
 
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("eCommerce API is running!"));
+
+app.use(errorHandler);
 
 app.listen(port, () =>
   console.log(`Main app listening at http://localhost:${port}`)
