f71c39d9f94b90b9b6a9a2c6dd1627acad9b896a
Daria Kozlova <kozloffa.daria@gmail.com>
2026-01-08 20:33:29 +0100

    Add TypeScript types for CORS middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import "#db";
 import {errorHandler} from "#middlerware";
-import cors from "cors";
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 8080;
