f65b400d3400e4e3e3c7797d9d090ad0ae184268
Daria Kozlova <kozloffa.daria@gmail.com>
2026-01-09 12:21:36 +0100

    Add Zod schemas for Product: ProductInputSchema and ProductSchema

---
+++ src/schemas/product.ts
@@ -0,0 +1,20 @@
+import { z } from "zod/v4";
+import { Types } from "mongoose";
+
+// 1️⃣ Input schema — for POST/PUT req
+export const productInputSchema = z.strictObject({
+  name: z.string({ error: "ProductName must be a string" }).min(1, {message: "Product name is required"}),
+  description: z.string({ error: "Description must be a string" }).min(1, {message: "Description is required"}),
+  price: z.number({ error: "Price must be a number" }).min(0, {message: "Price must be a positive number"}),
+  categoryId: z.string().min(1, {message: "CategoryId is required"}),
+});
+
+// 2️⃣ Full schema — for DB data
+export const productSchema = z.strictObject({
+  _id: z.instanceof(Types.ObjectId),
+  ...productInputSchema.shape,
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  __v: z.number(),
+});
+
