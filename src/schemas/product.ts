6ef37ebc61f70144e8a43ceaae2fb73e5053b408
Daria Kozlova <kozloffa.daria@gmail.com>
2026-01-10 15:12:49 +0100

    stock added to productSchema

--- src/schemas/product.ts
+++ src/schemas/product.ts
@@ -20,6 +20,7 @@ export const productSchema = z.strictObject({
   name: z.string(),
   description: z.string(),
   price: z.number(),
+  stock: z.number(),
   categoryId: z.instanceof(Types.ObjectId),
   createdAt: z.date(),
   updatedAt: z.date(),
