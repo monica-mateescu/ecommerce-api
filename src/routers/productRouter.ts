949577b0a47eb03047b008e5e342318b3d2e3f14
Daria Kozlova <kozloffa.daria@gmail.com>
2026-01-17 20:33:50 +0100

    docs: add swagger docs for GET /products

--- src/routers/productRouter.ts
+++ src/routers/productRouter.ts
@@ -1,3 +1,22 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Product:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *         name:
+ *           type: string
+ *         description:
+ *           type: string
+ *         price:
+ *           type: number
+ *         categoryId:
+ *           type: string
+ */
+
 import { Router } from "express";
 import {
   getProducts,
@@ -10,6 +29,27 @@ import { validateBodyZod, validateObjectIdParam } from "#middleware";
 import { productInputSchema } from "#schemas";
 
 const productRouter = Router();
+
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     summary: Get all products
+ *     parameters:
+ *       - in: query
+ *         name: categoryId
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: List of products
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Product'
+ */
 
 productRouter.get("/", getProducts);
 productRouter.get("/:id", validateObjectIdParam('id'), getProductById);
